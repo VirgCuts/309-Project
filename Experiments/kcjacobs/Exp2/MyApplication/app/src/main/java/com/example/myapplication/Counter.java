1bb20e08575351c12aa204ab3596d33bc4a32ae6
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-09-09 14:43:33 -0500

    first app. haven't gotten it to work yet

---
+++ Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/Counter.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Counter extends AppCompatActivity {
+
+    Button increaseBtn;
+    Button backBtn;
+    TextView numberTxt;
+
+    int counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+
+
+        increaseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                numberTxt.setText(String.valueOf(++counter));
+            }
+        });
+
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Counter.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
---
+++ Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button subtractButton, additionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+
+        subtractButton = findViewById(R.id.subtractButton);
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, Counter.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
