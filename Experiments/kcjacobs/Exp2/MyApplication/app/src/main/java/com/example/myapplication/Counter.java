710731d473171e2e4d3cc555fa28b5edd9eace71
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-09-09 23:19:26 -0500

    couple little changes

--- Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/Counter.java
+++ Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/Counter.java
@@ -19,7 +19,7 @@ public class Counter extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
+        setContentView(R.layout.activity_main);
 
 
 
--- Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Experiments/kcjacobs/Exp2/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout);
+        setContentView(R.layout.activity_main);
 
         subtractButton = findViewById(R.id.subtractButton);
 
