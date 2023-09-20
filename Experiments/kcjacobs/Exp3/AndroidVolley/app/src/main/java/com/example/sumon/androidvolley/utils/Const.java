24f1172ed4214347ecca42b20b6d37c52a5b532d
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-09-20 12:50:36 -0500

    Changes so I can see my mock server

--- Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -3,6 +3,6 @@ package com.example.sumon.androidvolley.utils;
 public class Const {
 	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_IMAGE = "http://sharding.org/outgoing/temp/testimg3.jpg";
+	public static final String URL_STRING_REQ = "https://b8600cf9-bbf4-4caf-b800-ac0cb69aa7f2.mock.pstmn.io/helloName";
+	public static final String URL_IMAGE = "https://tinemettejespersen.files.wordpress.com/2012/03/ffftextthisisanimage.gif";
 }
