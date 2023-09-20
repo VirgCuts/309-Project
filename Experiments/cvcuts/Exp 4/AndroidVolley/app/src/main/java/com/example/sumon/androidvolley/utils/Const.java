dd9f240eb46a2145d62bb5cccb279039d29f7faa
VirgCuts <cvcuts@iastate.edu>
2023-09-20 15:10:37 -0500

    Added a test image

--- Experiments/cvcuts/Exp 4/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/cvcuts/Exp 4/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -1,8 +1,8 @@
 package com.example.sumon.androidvolley.utils;
 
 public class Const {
-	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_JSON_OBJECT = "https://3bb97b8c-ed28-4ef1-b090-83a60f9cd73c.mock.pstmn.io/";
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_IMAGE = "http://sharding.org/outgoing/temp/testimg3.jpg";
+	public static final String URL_STRING_REQ = "https://stackoverflow.com/questions/66220146/how-to-use-jsontest-when-there-is-no-springbootapplication";
+	public static final String URL_IMAGE = "https://th.bing.com/th/id/R.34b6a78d81e6d811f5bab67b979574cc?rik=LVfLm2iG7lAwBg&riu=http%3a%2f%2fnewearthjourney.files.wordpress.com%2f2010%2f11%2fcardinal1.jpg&ehk=u3de6hD1AUZ2FrtHbvkBqhxZxvxn9zO2acqOhyxMTNk%3d&risl=&pid=ImgRaw&r=0";
 }
