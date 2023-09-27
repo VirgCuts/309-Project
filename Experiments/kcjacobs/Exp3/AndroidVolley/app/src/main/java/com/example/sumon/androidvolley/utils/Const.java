104ea89e0c99fb4cb8b86c4f06dbb9585dc3e5b4
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-09-27 14:50:17 -0500

    Experimenting with front end User Input

--- Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -3,6 +3,6 @@ package com.example.sumon.androidvolley.utils;
 public class Const {
 	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://b8600cf9-bbf4-4caf-b800-ac0cb69aa7f2.mock.pstmn.io/helloName";
+	public static final String URL_STRING_REQ = "https://b2866a82-9631-4386-8b72-465e3ef48af6.mock.pstmn.io/Object/Default";
 	public static final String URL_IMAGE = "https://tinemettejespersen.files.wordpress.com/2012/03/ffftextthisisanimage.gif";
 }
