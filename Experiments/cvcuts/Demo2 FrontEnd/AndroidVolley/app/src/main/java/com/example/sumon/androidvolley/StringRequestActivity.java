12e7f56a72bb61a4445946737cab43c1ca9da7cc
VirgCuts <cvcuts@iastate.edu>
2023-10-03 16:23:25 -0500

    Created a copy of Volley Frontend for Demo2, creating the search method.

---
+++ Experiments/cvcuts/Demo2 FrontEnd/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/StringRequestActivity.java
@@ -0,0 +1,101 @@
+package com.example.sumon.androidvolley;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.example.sumon.androidvolley.app.AppController;
+import com.example.sumon.androidvolley.utils.Const;
+
+public class StringRequestActivity extends Activity {
+
+    private String TAG = StringRequestActivity.class.getSimpleName();
+    private Button btnStringReq, btnBack;
+    private TextView msgResponse;
+    private ProgressDialog pDialog;
+
+    private EditText searchText;
+
+    // This tag will be used to cancel the request
+    private String tag_string_req = "string_req";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.string_request);
+
+        btnStringReq = (Button) findViewById(R.id.btnStringReq);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        msgResponse = (TextView) findViewById(R.id.msgResponse);
+        searchText =  findViewById(R.id.searchText);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.setCancelable(false);
+
+        btnStringReq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makeStringReq();
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StringRequestActivity.this,
+                        MainActivity.class));
+
+            }
+        });
+    }
+    //Checks edit text and compares it to the users in the data returns true of false depending on inclusion.
+    public boolean inDatabase() {
+
+        return false;
+    }
+    private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
+
+    /**
+     * Making json object request
+     * */
+    private void makeStringReq() {
+        showProgressDialog();
+
+        StringRequest strReq = new StringRequest(Method.GET, Const.URL_STRING_REQ, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, response.toString());
+                msgResponse.setText(response.toString());
+                hideProgressDialog();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                hideProgressDialog();
+            }
+        });
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+
+    }
+}
---
+++ Experiments/cvcuts/Demo2 FrontEnd/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -0,0 +1,8 @@
+package com.example.sumon.androidvolley.utils;
+
+public class Const {
+	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users";
+	public static final String URL_JSON_ARRAY = "https://9d5102cf-f39d-4814-8d2d-68622b5fc0e1.mock.pstmn.io/getArtists";
+	public static final String URL_STRING_REQ = "https://9d5102cf-f39d-4814-8d2d-68622b5fc0e1.mock.pstmn.io/getArtists";
+
+}
