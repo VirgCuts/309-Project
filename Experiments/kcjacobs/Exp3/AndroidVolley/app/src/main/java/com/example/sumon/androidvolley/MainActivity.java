8113fbad56087a9dda5b362937982fc0a81bb39a
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-10-04 18:54:09 -0500

    Working frontend with backend for leaderboard

--- Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/MainActivity.java
+++ Experiments/kcjacobs/Exp3/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends Activity implements OnClickListener {
                 break;
             case R.id.btnImageRequest:
                 startActivity(new Intent(MainActivity.this,
-                        ImageRequestActivity.class));
+                        LeaderboardActivity.class));
                 break;
             default:
                 break;
