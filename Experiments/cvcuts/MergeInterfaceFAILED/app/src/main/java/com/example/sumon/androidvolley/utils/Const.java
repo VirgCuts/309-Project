8b803cc1ae30c909710dee6224b586d77443614d
VirgCuts <cvcuts@iastate.edu>
2023-10-23 00:40:41 -0500

    Resolved problem with gradle not detecting POM file for dependencies

--- Experiments/cvcuts/MergeInterfaceFAILED/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++
@@ -1,8 +0,0 @@
-package com.example.sumon.androidvolley.utils;
-
-public class Const {
-	public static final String URL_JSON_OBJECT = "https://coms-309-022.class.las.iastate.edu";
-	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://b2866a82-9631-4386-8b72-465e3ef48af6.mock.pstmn.io/Object/Default";
-	public static final String URL_IMAGE = "https://tinemettejespersen.files.wordpress.com/2012/03/ffftextthisisanimage.gif";
-}
