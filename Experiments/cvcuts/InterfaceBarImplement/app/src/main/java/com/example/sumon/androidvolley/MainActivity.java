0fa2e89a239a20518cba701729ee6affa2eb4852
VirgCuts <cvcuts@iastate.edu>
2023-10-23 14:02:31 -0500

    Implemented a navbar in main, need to add buttons+ nav on each view

--- Experiments/cvcuts/InterfaceBarImplement/app/src/main/java/com/example/sumon/androidvolley/MainActivity.java
+++ Experiments/cvcuts/InterfaceBarImplement/app/src/main/java/com/example/sumon/androidvolley/MainActivity.java
@@ -1,45 +1,36 @@
 package com.example.sumon.androidvolley;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener {
-    private Button btnLeaderboard, btnArtists, btnWebsocket;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnLeaderboard = (Button) findViewById(R.id.btnleaderboard);
-        btnArtists = (Button) findViewById(R.id.btnArtists);
-        btnWebsocket = (Button) findViewById(R.id.btnWebsocket);
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        btnLeaderboard.setOnClickListener(this);
-        btnArtists.setOnClickListener(this);
-        btnWebsocket.setOnClickListener(this);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btnleaderboard:
-                startActivity(new Intent(MainActivity.this,
-                        LeaderboardActivity.class));
-                break;
-            case R.id.btnArtists:
-                startActivity(new Intent(MainActivity.this,
-                        ArtistActivity.class));
-                break;
-            case R.id.btnWebsocket:
-                startActivity(new Intent(MainActivity.this,
-                        MessageBoard.class));
-                break;
-            default:
-                break;
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
-
 }
+
