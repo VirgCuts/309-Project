144a4e3881ceb13d107290dde3513ba373c5641f
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-10-30 13:45:19 -0500

    New endgame popup with UI and Implementation

--- Experiments/cvcuts/Game/app/src/main/java/com/example/game/gameFiles/SinglePlayerGame.java
+++ Experiments/cvcuts/Game/app/src/main/java/com/example/game/gameFiles/SinglePlayerGame.java
@@ -1,7 +1,12 @@
 package com.example.game.gameFiles;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,10 +20,14 @@ public class SinglePlayerGame extends AppCompatActivity implements GameViewInter
     private long startTime;
     private long endTime;
     private TextView timerTextView;
+
+    private Button endGameButton;
     private Handler handler = new Handler();
     private int seconds = 0;
 
     private int points = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +43,17 @@ public class SinglePlayerGame extends AppCompatActivity implements GameViewInter
         EditText r3c2 = findViewById(R.id.r3c2);
         EditText r3c3 = findViewById(R.id.r3c3);
         EditText r1c1 = findViewById(R.id.r1c1);
+
+
+
+        //This is just used to test the end screen
+        endGameButton = findViewById(R.id.endGameButton);
+        endGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endGame();
+            }
+        });
 
         timerTextView = findViewById(R.id.timer);
         // Start the timer
@@ -115,9 +135,50 @@ public class SinglePlayerGame extends AppCompatActivity implements GameViewInter
     @Override
     public void endGame() {
         // End the game, display final score, stop the timer, etc.
+        //The following board is just for testing
+        String[][] board = {{"Kanye","Kanye","Kanye"},{"Kanye","Kanye","Kanye"},{"Kanye","Kanye","Kanye"}};
+        showWinnerDialog("1",board);
     }
     @Override
     public void showEndGameScore(int score) {
         // Show the player's final score
+    }
+    //NOTE: winnerBoard will be changed to a new custom class called Board
+    private void showWinnerDialog(String winner, String[][] winnerBoard) {
+        // Create and show the custom dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.end_game_popup, null);
+        builder.setView(dialogView);
+
+        // Set Winner Text
+        TextView winnerText = dialogView.findViewById(R.id.winnerText);
+        winnerText.setText("Player " + winner + " wins");  // Change 'Player X' dynamically based on game result
+
+        // Populate the winner's board grid
+        int[] cellIds = {
+                R.id.r1c1, R.id.r1c2, R.id.r1c3,
+                R.id.r2c1, R.id.r2c2, R.id.r2c3,
+                R.id.r3c1, R.id.r3c2, R.id.r3c3
+        };
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                TextView cell = dialogView.findViewById(cellIds[i * 3 + j]);
+                cell.setText(winnerBoard[i][j]);
+            }
+        }
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        // Add restart button functionality
+        Button restartButton = dialogView.findViewById(R.id.restartButton);
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Restart your game here
+                alertDialog.dismiss();
+            }
+        });
     }
 }
