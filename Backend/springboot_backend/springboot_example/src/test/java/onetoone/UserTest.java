cf3992dc7f0ed83058566cf1867102fcbd0e4784
slickteig-03 <samlickteig@icloud.com>
2023-11-27 00:25:25 -0600

    quick test change

--- Backend/springboot_backend/springboot_example/src/test/java/onetoone/UserTest.java
+++ Backend/springboot_backend/springboot_example/src/test/java/onetoone/UserTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Scanner;
 
+import onetoone.Users.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -17,19 +18,16 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class UserTest {
 
-    float loanAmount;
-    float downPayment;
-    float availableFunds;
-    boolean expectApproved;
-    String expectedMessage;
+    String name;
 
-    public UserTest(float loanAmount, float downPayment, float availableFunds, boolean expectApproved,
-                   String expectedMessage) {
-        this.loanAmount = loanAmount;
-        this.downPayment = downPayment;
-        this.availableFunds = availableFunds;
-        this.expectApproved = expectApproved;
-        this.expectedMessage = expectedMessage;
+    String password;
+
+    int highscore;
+
+    public UserTest(String name, String password, int highscore) {
+        name = name;
+        password = password;
+        highscore = highscore;
     }
 
     // @Parameters(name = "Run {index}: loanAmount={0}, downPayment={1},
@@ -45,46 +43,28 @@ public class UserTest {
     //
     // });
     // }
-
     @Parameters
     public static Collection<Object[]> getParameters() {
         Collection<Object[]> retList = new ArrayList<Object[]>();
-        try {
-            Scanner in = new Scanner(new File("dataFile.txt"));
+        // split the line using delimiter and then create the test-case object
+        Object[] d = new Object[3];
+        d[0] = "Sam";
+        d[1] = "password";
+        d[2] = 1000;
 
-            // Read as many lines as there are in the file
-            while (in.hasNextLine()) {
-                String l = in.nextLine();
 
-                // split the line using delimiter and then create the test-case object
-                String dataArray[] = l.split(",");
-                Object[] d = new Object[5];
-                d[0] = Float.parseFloat(dataArray[0]);
-                d[1] = Float.parseFloat(dataArray[1]);
-                d[2] = Float.parseFloat(dataArray[2]);
-                d[3] = Boolean.parseBoolean(dataArray[3]);
-                d[4] = dataArray[4];
-
-                // add the test data into the arraylist
-                retList.add(d);
-
-            } // end of while
-            in.close();
-
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-
-        }
-
+        // add the test data into the arraylist
+        retList.add(d);
         // return all the test-cases
         return retList;
     }
 
-
     @Test  // run this for each test-case in the above collection
     public void testRequestLoan() throws Throwable {
-        // Given
-
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        user.setAllHighScores(highscore);
+        assertEquals(user.getHighScore(), highscore);
     }
 }
