1171a122575aa44e30e12791d7e276757228a7a0
Conor O'Shea <coshea@iastate.edu>
2023-12-04 14:32:48 -0600

    Making team multiplayer websocket

---
+++ Backend/springboot_backend/springboot_example/src/main/java/onetoone/Websocket/TeamCombinedJSON.java
@@ -0,0 +1,27 @@
+package onetoone.Websocket;
+
+import onetoone.Users.Board;
+
+public class TeamCombinedJSON {
+    private String name1;
+    private String name2;
+    private String name3;
+    private String name4;
+    private Board board;
+
+    public String getName1() {
+        return name1;
+    }
+    public String getName2() {
+        return name2;
+    }
+    public String getName3() {
+        return name3;
+    }
+    public String getName4() {
+        return name4;
+    }
+    public Board getBoard() {
+        return board;
+    }
+}
