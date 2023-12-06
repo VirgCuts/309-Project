1e06a6054825280e6291692fc2ad889502992e36
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-12-06 14:33:10 -0600

    some new tests

---
+++ Frontend/app/src/androidTest/java/com/example/sumon/androidvolley/KeenanMainActivityTest2.java
@@ -0,0 +1,119 @@
+package com.example.sumon.androidvolley;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class KeenanMainActivityTest2 {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest2() throws InterruptedException {
+        Thread.sleep(50);
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.username_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.my_drawer_layout),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("Keenan"), closeSoftKeyboard());
+        Thread.sleep(50);
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.password_input),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.my_drawer_layout),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatEditText2.perform(replaceText("password1"), closeSoftKeyboard());
+        Thread.sleep(50);
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.login), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.my_drawer_layout),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+        Thread.sleep(50);
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(R.id.btnStudy),
+                        isDisplayed()));
+        appCompatButton2.perform(click());
+        Thread.sleep(50);
+        ViewInteraction appCompatButton3 = onView(
+                allOf(withId(R.id.button),
+                        isDisplayed()));
+        appCompatButton3.perform(click());
+        Thread.sleep(50);
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.songGuess),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.my_drawer_layout),
+                                        0),
+                                3),
+                        isDisplayed()));
+        appCompatEditText3.perform(replaceText("1"), closeSoftKeyboard());
+        Thread.sleep(50);
+        ViewInteraction appCompatButton4 = onView(
+                allOf(withId(R.id.showAllAnswers), withText("Show Artist songs"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.my_drawer_layout),
+                                        0),
+                                8),
+                        isDisplayed()));
+        appCompatButton4.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
