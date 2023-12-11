596d33c3d23a1d7172bac5a8d52498b8a2e0cd3c
VirgCuts <cvcuts@iastate.edu>
2023-12-10 22:30:30 -0600

    Fixing

--- Frontend/app/src/main/java/com/example/sumon/androidvolley/sendBoard.java
+++ Frontend/app/src/main/java/com/example/sumon/androidvolley/sendBoard.java
@@ -58,58 +58,58 @@ public class sendBoard {
         return null; // Or throw an exception
     }
 
-//
-//    /**
-//     * Edits a column with the specified values.
-//     *
-//     * @param c       The column index to edit.
-//     * @param text    The text value for the column.
-//     * @param subject The subject value for the column.
-//     * @param check   The check value for the column.
-//     * @param keyword The keyword value for the column.
-//     */
-//    public void editCol(int c, String text, String subject, String check, String keyword) {
-//        col[c][0] = text;
-//        col[c][1] = subject;
-//        col[c][2] = check;
-//        col[c][3] = keyword;
-//    }
-//
-//    /**
-//     * Edits a row with the specified values.
-//     *
-//     * @param c       The row index to edit.
-//     * @param text    The text value for the row.
-//     * @param subject The subject value for the row.
-//     * @param check   The check value for the row.
-//     * @param keyword The keyword value for the row.
-//     */
-//    public void editRow(int c, String text, String subject, String check, String keyword) {
-//        row[c][0] = text;
-//        row[c][1] = subject;
-//        row[c][2] = check;
-//        row[c][3] = keyword;
-//    }
-//    /**
-//     * Retrieves a value from a specific column at a given check index.
-//     *
-//     * @param co    The column index.
-//     * @param check The check index.
-//     * @return The value at the specified column and check index.
-//     */
-//    public String getCol(int co, int check) {
-//        return col[co][check];
-//    }
-//    /**
-//     * Retrieves a value from a specific row at a given check index.
-//     *
-//     * @param ro    The row index.
-//     * @param check The check index.
-//     * @return The value at the specified row and check index.
-//     */
-//    public String getRow(int ro, int check) {
-//        return row[ro][check];
-//    }
+
+    /**
+     * Edits a column with the specified values.
+     *
+     * @param c       The column index to edit.
+     * @param text    The text value for the column.
+     * @param subject The subject value for the column.
+     * @param check   The check value for the column.
+     * @param keyword The keyword value for the column.
+     */
+    public void editCol(int c, String text, String subject, String check, String keyword) {
+        col[c][0] = text;
+        col[c][1] = subject;
+        col[c][2] = check;
+        col[c][3] = keyword;
+    }
+
+    /**
+     * Edits a row with the specified values.
+     *
+     * @param c       The row index to edit.
+     * @param text    The text value for the row.
+     * @param subject The subject value for the row.
+     * @param check   The check value for the row.
+     * @param keyword The keyword value for the row.
+     */
+    public void editRow(int c, String text, String subject, String check, String keyword) {
+        row[c][0] = text;
+        row[c][1] = subject;
+        row[c][2] = check;
+        row[c][3] = keyword;
+    }
+    /**
+     * Retrieves a value from a specific column at a given check index.
+     *
+     * @param co    The column index.
+     * @param check The check index.
+     * @return The value at the specified column and check index.
+     */
+    public String getCol(int co, int check) {
+        return col[co][check];
+    }
+    /**
+     * Retrieves a value from a specific row at a given check index.
+     *
+     * @param ro    The row index.
+     * @param check The check index.
+     * @return The value at the specified row and check index.
+     */
+    public String getRow(int ro, int check) {
+        return row[ro][check];
+    }
 
 
     /**
@@ -122,15 +122,15 @@ public class sendBoard {
     private boolean isValidPosition(int row, int col) {
         return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE;
     }
-//    /**
-//     * Retrieves the entire grid of the board.
-//     *
-//     * @return The 2D array representing the board's grid.
-//     */
-//    public String[][] getGrid() {
-//
-//        return griddle;
-//    }
+    /**
+     * Retrieves the entire grid of the board.
+     *
+     * @return The 2D array representing the board's grid.
+     */
+    public String[][] getGrid() {
+
+        return griddle;
+    }
     /**
      * Generates a string representation of the board's grid.
      *
