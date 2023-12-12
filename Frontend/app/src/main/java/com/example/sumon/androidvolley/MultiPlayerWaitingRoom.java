26218b28704a6f4c4090633265a0a864be872c6c
kcjacobs13 <keenanjacobs0213@gmail.com>
2023-12-12 14:02:29 -0600

    hotifx

--- Frontend/app/src/main/java/com/example/sumon/androidvolley/MultiPlayerWaitingRoom.java
+++ Frontend/app/src/main/java/com/example/sumon/androidvolley/MultiPlayerWaitingRoom.java
@@ -217,7 +217,8 @@ public class MultiPlayerWaitingRoom extends AppCompatActivity implements WebSock
                     intent.putExtra("ERROR_MESSAGE", "Lobby is full, sorry");
                     startActivity(intent);
                     finish();
-                }else if(message.contains("Start game: ")){
+                }else if(message.contains("Start game")){
+                    Log.d("MESSAGE",message);
                     Intent intent = new Intent(MultiPlayerWaitingRoom.this, MultiPlayerGame.class);
                     intent.putExtra("PLAYER", username);
                     startActivity(intent);
